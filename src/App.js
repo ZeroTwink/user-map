ec4c678f78ae57dfee151fc83f06dbab5288fb20
3D <ZeroTwink@gmail.com>
2018-03-14 16:21:20 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -19,84 +19,106 @@ class App extends Component {
         };
     }
 
+    drawMap(users) {
+        let count = users.length;
+        let features = new Array(count);
+
+        for (let i = 0; i < count; ++i) {
+            features[i] = new ol.Feature({
+                'geometry': new ol.geom.Point([
+                    users[i]['geometry']['coordinates'][0] + 4188426.7147939987,
+                    users[i]['geometry']['coordinates'][1] + 7508764.236877314
+                ]),
+                'i': i
+            });
+
+            features[i].setStyle(
+                new ol.style.Style({
+                    image: new ol.style.Circle({
+                        radius: 7,
+                        fill: new ol.style.Fill({color: users[i]['properties']['color']})
+                    })
+                })
+            );
+        }
+
+        let vectorSource = new ol.source.Vector({
+            features: features,
+            wrapX: false
+        });
+        let vector = new ol.layer.Vector({
+            source: vectorSource
+        });
+
+        let map = new ol.Map({
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                }),
+                vector
+            ],
+            target: document.getElementById('map'),
+            view: new ol.View({
+                center: [ 4188426.7147939987, 7508764.236877314 ],
+                zoom: 18
+            })
+        });
+
+        let popup = new ol.Overlay({
+            element: document.getElementById('popup'),
+            positioning: 'bottom-center'
+        });
+        map.addOverlay(popup);
+
+        map.on('click', function(evt) {
+            let feature = map.forEachFeatureAtPixel(evt.pixel,
+                function(feature) {
+                    return feature;
+                });
+            if (feature) {
+                console.log(users[feature.get('i')]['properties']['userName']);
+                popup.setPosition([
+                    users[feature.get('i')]['geometry']['coordinates'][0] + 4188426.7147939987,
+                    users[feature.get('i')]['geometry']['coordinates'][1] + 7508764.236877314
+                ]);
+            } else {
+                console.log(0);
+            }
+        });
+
+        map.on('pointermove', function(evt) {
+            if (evt.dragging) {
+                return;
+            }
+            let pixel = map.getEventPixel(evt.originalEvent);
+            let hit = map.hasFeatureAtPixel(pixel);
+            if (hit) {
+                map.getTarget().style.cursor = 'pointer';
+            } else {
+                map.getTarget().style.cursor = '';
+            }
+        });
+    }
+
     componentDidMount() {
         axios.get("http://localhost:3007/features")
             .then(res => {
-                console.log(res);
-                this.setState({
-                    users: res.data.features.slice(900)
-                });
-
                 let users = res.data.features.slice(900);
 
-                var count = users.length;
-                var features = new Array(count);
-                for (let i = 0; i < count; ++i) {
-                    features[i] = new ol.Feature({
-                        'geometry': new ol.geom.Point([users[i].geometry.coordinates[0] + 4188426.7147939987, users[i].geometry.coordinates[1] + 7508764.236877314]),
-                        'i': i
-                    });
-
-                    features[i].setStyle(
-                        new ol.style.Style({
-                            image: new ol.style.Circle({
-                                radius: 10,
-                                fill: new ol.style.Fill({color: users[i]['properties']['color']})
-                            })
-                        })
-                    );
-                }
-
-                var vectorSource = new ol.source.Vector({
-                    features: features,
-                    wrapX: false
-                });
-                var vector = new ol.layer.Vector({
-                    source: vectorSource
+                this.setState({
+                    users: users
                 });
 
-                var map = new ol.Map({
-                    layers: [
-                        new ol.layer.Tile({
-                            source: new ol.source.OSM()
-                        }),
-                        vector
-                    ],
-                    target: document.getElementById('map'),
-                    view: new ol.View({
-                        center: [ 4188426.7147939987, 7508764.236877314 ],
-                        zoom: 18
-                    })
-                });
-
-                map.on('click', function(evt) {
-                    var feature = map.forEachFeatureAtPixel(evt.pixel,
-                        function(feature) {
-                            return feature;
-                        });
-                    if (feature) {
-                        console.log(users[feature.get('i')]['properties']['userName']);
-                    } else {
-                        console.log(0);
-                    }
-                });
-
-                map.on('pointermove', function(evt) {
-                    if (evt.dragging) {
-                        return;
-                    }
-                    var pixel = map.getEventPixel(evt.originalEvent);
-                    var hit = map.hasFeatureAtPixel(pixel);
-                    if (hit) {
-                        map.getTarget().style.cursor = 'pointer';
-                    } else {
-                        map.getTarget().style.cursor = '';
-                    }
-                });
+                this.drawMap(users);
             })
             .catch(error => {
                 console.log(error);
             });
+    }
+
+    sert4(i, e) {
+        console.log(this.state.users[i]['properties']['userName'], i);
+        console.log(this.state.users[i]['geometry']['coordinates']);
     }
 
     render() {
@@ -104,13 +126,13 @@ class App extends Component {
             <div>
                 <Drawer open={true} width={310}>
                     <List>
-                        {this.state.users.map((e) => {
+                        {this.state.users.map((e, i) => {
                             return (
                                 <ListItem
                                     key={e.id}
                                     hoverColor={blue100}
                                     leftAvatar={<Avatar src={e.properties.avatar} />}
-                                    onClick={(e) => {console.log(e)}}
+                                    onClick={this.sert4.bind(this, i)}
                                     secondaryText={e.properties.email}>
                                     {e.properties.userName}
                                 </ListItem>
@@ -120,8 +142,12 @@ class App extends Component {
                 </Drawer>
 
                 <div id="content">
-                    <div id="map">
+                    <div id="map" />
+                </div>
 
+                <div style={{display: "none"}}>
+                    <div id="popup">
+                        dfsfdfds
                     </div>
                 </div>
             </div>
